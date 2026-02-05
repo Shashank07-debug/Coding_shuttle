8e796a509c58d6d6f1ae88c6c838a93dd13f995f
shashank007 <shashankmn066@gmail.com>
2026-02-05 14:49:31 +0530

    first commit

---
+++ cakeBaker/src/main/java/com/example/cakeBaker/CakeBakerApplication.java
@@ -0,0 +1,13 @@
+package com.example.cakeBaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CakeBakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CakeBakerApplication.class, args);
+	}
+
+}
